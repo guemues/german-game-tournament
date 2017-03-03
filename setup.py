551dd1ccf61cdd69f219fae90109ab7544155043
Orçun Gümüş <orcungumus@gmail.com>
2017-03-03 13:54:46 +0300

    Pypi update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='germantournament',
+  packages=['germantournament'],  # this must be the same as the name above
+  version='0.1',
+  description='A package for help schedule tournaments',
+  author='Orcun Gumus',
+  author_email='orcungumus@gmail.com',
+  url='https://github.com/guemues/german-game-tournament',  # use the URL to the github repo
+  download_url='https://github.com/guemues/german-game-tournament/archive/0.1.tar.gz',  # I'll explain this in a second
+  keywords=['tournament', 'german', 'game'],  # arbitrary keywords
+  classifiers=[],
+)
--- tests/test_SevenWonders.py
+++ tests/test_SevenWonders.py
@@ -42,7 +42,7 @@ class TestSevenWonders(TestCase):
         self.tournament._fix_players(player_count_in_one_game=2)
         self.assertEqual(len(self.tournament.players) % 4, 0)
 
-    def test_schedule(self):
+    def test_schedule_for_2(self):
         self.assertEqual(self.tournament.players, [])
         self.tournament.set_players(list(range(0, 4)))
         self.assertCountEqual(
@@ -57,10 +57,28 @@ class TestSevenWonders(TestCase):
              [0, 2], [1, 3]])
 
         self.tournament.set_players(list(range(0, 5)))
-        print(self.tournament.schedule(player_count_in_one_game=2))
         self.assertCountEqual(
             self.tournament.schedule(player_count_in_one_game=2),
             [[0, 1], [2, 3],
              [0, 2], [1, 3],
              [4, 5], [6, 7],
              [4, 6], [5, 7]])
+
+    def test_schedule_for_4(self):
+        self.assertEqual(self.tournament.players, [])
+        self.tournament.set_players(list(range(0, 16)))
+        self.assertCountEqual(
+            self.tournament.schedule(player_count_in_one_game=4),
+            [[0, 1, 2, 3],  [4, 5, 6, 7],  [8, 9, 10, 11], [12, 13, 14, 15],
+             [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]])
+
+    def test_schedule_for_4x2(self):
+        self.assertEqual(self.tournament.players, [])
+        self.tournament.set_players(list(range(0, 32)))
+        self.assertCountEqual(
+            self.tournament.schedule(player_count_in_one_game=4),
+            [[0, 1, 2, 3],  [4, 5, 6, 7],  [8, 9, 10, 11], [12, 13, 14, 15],
+             [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15],
+             [16, 17, 18, 19], [20, 21, 22, 23], [24, 25, 26, 27], [28, 29, 30, 31],
+             [16, 20, 24, 28], [17, 21, 25, 29], [18, 22, 26, 30], [19, 23, 27, 31]
+             ])
